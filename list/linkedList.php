f804855f8f3bd0d0e7970cecef963750ba71a45f
arase <arase.taisuke@gmail.com>
2021-11-06 23:23:48 +0900

    list に reverseEven 関数を実装

--- list/linkedList.php
+++ list/linkedList.php
@@ -81,7 +81,7 @@ class LinkedList
 
     public function reverseRecursive()
     {
-        function _reverseRecursive($currentNode = null, $prevNode = null)
+        function _reverseRecursive(Node $currentNode = null, Node $prevNode = null)
         {
             if (!$currentNode) {
                 return $prevNode;
@@ -94,6 +94,40 @@ class LinkedList
         }
 
         $this->head = _reverseRecursive($this->head, null);
+    }
+
+    /**
+     * 1, 4, 6, 8, 9 => 1, 8, 6, 4, 9
+     */
+    public function reverseEven()
+    {
+        function _reverseEven(Node $head = null, Node $prevNode = null): ?Node
+        {
+            if (!$head) {
+                return null;
+            }
+
+            $currentNode = $head;
+
+            while ($currentNode && $currentNode->data % 2 === 0) {
+                $nextNode = $currentNode->next;
+                $currentNode->next = $prevNode;
+
+                $prevNode = $currentNode;
+                $currentNode = $nextNode;
+            }
+
+            if ($currentNode !== $head) {
+                $head->next = $currentNode;
+                _reverseEven($currentNode, null);
+                return $prevNode;
+            } else {
+                $head->next = _reverseEven($head->next, $head);
+                return $head;
+            }
+        }
+
+        $this->head = _reverseEven($this->head, null);
     }
 }
 
@@ -118,4 +152,19 @@ $list->print();
 echo PHP_EOL;
 
 $list->reverseRecursive();
+$list->print();
+
+echo PHP_EOL;
+
+$list = new LinkedList();
+$list->append(2);
+$list->append(4);
+$list->append(6);
+$list->append(1);
+$list->append(2);
+$list->append(4);
+$list->append(6);
+$list->append(1);
+
+$list->reverseEven();
 $list->print();

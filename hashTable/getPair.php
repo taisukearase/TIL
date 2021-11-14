3d9e25ef60aa1e5f278db23da7c98adaccfc610c
arase <arase.taisuke@gmail.com>
2021-11-14 23:56:50 +0900

    シンメトリックのペアを探す関数を実装

--- hashTable/getPair.php
+++ hashTable/getPair.php
@@ -34,11 +34,43 @@ function getPairHalfSum(array $nums)
     }
 }
 
+function getSymmetryPair(array $nums)
+{
+    $cache = [];
+    $result = [];
+    foreach ($nums as [$key, $value]) {
+        $cachedValue = $cache[$value] ?? null;
+        if ($cachedValue === null) {
+            $cache[$key] = $value;
+            continue;
+        }
+        if ($cachedValue === $key) {
+            $result[] = [$value, $key];
+        }
+    }
+
+    return $result;
+}
+
 $list = [11, 2, 5, 9, 10, 3];
 $result = getPair($list, target: 12);
 
 var_dump($result);
 
 $result = getPairHalfSum($list);
+
+var_dump($result);
+
+echo PHP_EOL;
+
+$list = [
+    [1, 2],
+    [3, 5],
+    [4, 7],
+    [5, 3],
+    [7, 4],
+];
+
+$result = getSymmetryPair($list);
 
 var_dump($result);

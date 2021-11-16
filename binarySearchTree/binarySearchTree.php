abfa792f7a3a38fe31f3decfa33b20d40896e763
arase <arase.taisuke@gmail.com>
2021-11-16 23:44:56 +0900

    inOrder と search の実装

--- binarySearchTree/binarySearchTree.php
+++ binarySearchTree/binarySearchTree.php
@@ -25,6 +25,34 @@ function insert(?Node $node, $value): Node
     return $node;
 }
 
+function inOrder(?Node $node)
+{
+    if ($node) {
+        inOrder($node->left);
+        echo $node->value;
+        inOrder($node->right);
+    }
+}
+
+function search(?Node $node, int $value): bool
+{
+    if (!$node) {
+        return false;
+    }
+
+    if ($node->value === $value) {
+        return true;
+    }
+    
+    if ($node->value > $value) {
+        return search($node->left, $value);
+    }
+
+    if ($node->value < $value) {
+        return search($node->right, $value);
+    }
+}
+
 $root = null;
 $root = insert($root, 3);
 $root = insert($root, 6);
@@ -37,3 +65,7 @@ $root = insert($root, 2);
 echo $root->right->left->value;
 echo PHP_EOL;
 echo $root->left->right->value;
+echo PHP_EOL;
+echo inOrder($root);
+echo PHP_EOL;
+echo search($root, 1);

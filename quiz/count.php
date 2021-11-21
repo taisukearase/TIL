3643f85043d1196f734717cb89bfc7522a91fb95
arase <arase.taisuke@gmail.com>
2021-11-22 01:38:53 +0900

    最も多く出現する文字列をカウントする関数を実装

---
+++ quiz/count.php
@@ -0,0 +1,31 @@
+<?php
+// Input :'This is a pen. This is an apple. Applepen'
+// Output:('p', 6)
+
+function countCharsV1(string $input): array
+{
+    $lowerStr = strtolower($input);
+    $testStr  = str_replace(search: ' ', replace: '', subject: $lowerStr);
+    $strChars = str_split($testStr);
+
+    $resultArr = [];
+    foreach ($strChars as $char) {
+        $resultArr[$char] = substr_count($testStr, $char);
+    }
+
+    $maxCount = max($resultArr);
+    $maxKeys  = array_keys($resultArr, $maxCount);
+
+    return [$maxKeys[0], $maxCount];
+}
+
+$input = 'This is a pen. This is an apple. Applepen';
+$expected = ['p', 6];
+
+$actual = countCharsV1($input);
+
+$result = $actual == $expected;
+
+var_dump($actual);
+var_dump($expected);
+var_dump($result);
